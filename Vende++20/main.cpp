a701fa2598adf092c6151141190fc8c2d70de2f7
joaopedrofump <joaofurriel@mbp-de-joao-7.home>
2016-05-19 15:26:05 +0100

    hg

--- Vende++20/main.cpp
+++ Vende++20/main.cpp
@@ -29,7 +29,6 @@ int main(){
      cout << supermercado.getMaxClienteId() << endl;*/
     
     
-    
     cout << "Informacao da loja '" << loja << " do supermercado Vende++:" << endl;
     cout << supermercado << endl;  // mostra estatisticas da loja
     opcoesIniciais(supermercado);
